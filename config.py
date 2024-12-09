7ddf0a41b20d4138f8886fecb008378326432cc7
Daniil <49530059+Zhmuda@users.noreply.github.com>
2024-12-09 16:24:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 class Config:
     SECRET_KEY = 'secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CELERY_BROKER_URL = 'redis://localhost:6379/0'
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+    SWAGGER = {
+        'title': 'Transaction API',
+        'uiversion': 3
+    }
